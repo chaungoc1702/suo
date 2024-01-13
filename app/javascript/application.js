62284438c0c56701ad7ef2683c8e0251ab4df294
chaungoc <chaungoc1702@gmail.com>
2024-01-13 22:29:55 +0900

    DesignPink

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const toggleColor = (target) => {
+  if (target.style.color |="red") {
+  target.style.color = "red"
+  } else {
+  target.style.color = "blue"
+  }
+  }
