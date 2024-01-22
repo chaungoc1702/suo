f729d1399c50abc55aacc21a26304ea608779783
chaungoc <chaungoc1702@gmail.com>
2024-01-22 13:03:42 +0900

    Updated lessons index page Added images

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,3 +9,28 @@ const toggleColor = (target) => {
   target.style.color = "blue"
   }
   }
+#chucnangtimkiembaihoc(khonghoatdong)
+<script>
+const searchInput = document.getElementById('searchInput');
+const searchIcon = document.querySelector('.fa-search');
+
+searchIcon.addEventListener('click', function() {
+  searchInput.focus();
+});
+
+searchInput.addEventListener('input', function() {
+  const searchValue = this.value.toLowerCase();
+  const tableRows = document.querySelectorAll('#lessonTable tbody tr');
+
+  tableRows.forEach(row => {
+    const lessonName = row.querySelector('td:first-child').textContent.toLowerCase();
+    const lessonDescription = row.querySelector('td:nth-child(2)').textContent.toLowerCase();
+
+    if (lessonName.includes(searchValue) || lessonDescription.includes(searchValue)) {
+      row.style.display = '';
+    } else {
+      row.style.display = 'none';
+    }
+  });
+});
+</script>
