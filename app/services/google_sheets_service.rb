b4068aac8d1eac8d25f60cd46a3129cfcb38116b
chaungoc <chaungoc1702@gmail.com>
2024-05-10 10:23:15 +0900

    ggsheet

--- app/services/google_sheets_service.rb
+++ app/services/google_sheets_service.rb
@@ -3,22 +3,30 @@ require 'googleauth'
 require 'googleauth/stores/file_token_store'
 
 class GoogleSheetsService
-  OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
-  APPLICATION_NAME = 'SuperJLPT'.freeze
-  CREDENTIALS_PATH = 'secrets/superjlpt-6f080cf0ca49.json'.freeze
+  OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+  APPLICATION_NAME = 'SuperJLPT'
+  CREDENTIALS_PATH = 'secrets/superjlpt-6f080cf0ca49.json'
+  SHEET_ID = '1G3B6f2fRWfawcyzFghcQlKmdAIX5JjYNauuto7cvSO0'
 
-  def get_data
-    # Initialize the API
-    service = Google::Apis::SheetsV4::SheetsService.new
-    service.client_options.application_name = APPLICATION_NAME
-    service.authorization = authorizer
+  def get_lessons
+    get_data('Lessons', 'A1:D5')
+  end
 
-    # Fetch data from the sheet
-    spreadsheet_id = '1G3B6f2fRWfawcyzFghcQlKmdAIX5JjYNauuto7cvSO0'
-    range = 'Lessons!A1:D5'
-    response = service.get_spreadsheet_values(spreadsheet_id, range)
-    puts 'Data from the sheet:'
-    puts response.values
+  def get_data(sheet, range)
+    range = "#{sheet}!#{range}"
+    response = api.get_spreadsheet_values(SHEET_ID, range)
+    response.values
+  end
+
+  private
+
+  def api
+    @api ||= begin
+      api = Google::Apis::SheetsV4::SheetsService.new
+      api.client_options.application_name = APPLICATION_NAME
+      api.authorization = authorizer
+      api
+    end
   end
 
   def authorizer
@@ -26,18 +34,8 @@ class GoogleSheetsService
       json_key_io: File.open(CREDENTIALS_PATH),
       scope: Google::Apis::SheetsV4::AUTH_SPREADSHEETS
     )
-    #
-    # client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
-    # token_store = Google::Auth::Stores::FileTokenStore.new(file: 'token.yaml')
-    # authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
-    # user_id = 'default'
-    # credentials = authorizer.get_credentials(user_id)
-    # if credentials.nil?
-    #   url = authorizer.get_authorization_url(base_url: OOB_URI)
-    #   puts "Open the following URL in your browser and enter the resulting code after authorization:\n" + url
-    #   code = gets
-    #   credentials = authorizer.get_and_store_credentials_from_code(user_id: user_id, code: code, base_url: OOB_URI)
-    # end
-    # credentials
   end
 end
+
+# service = GoogleSheetsService.new
+# data = service.get_data('Lessons', 'A1:D5')
